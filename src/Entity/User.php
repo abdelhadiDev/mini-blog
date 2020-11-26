9ccfbbe5e50086771c2e06ffb4c50795371317ff
Abdelhadi <Abdel@macairabdel.home>
2020-11-26 14:03:38 +0100

    Add api plateform ressource annontation

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -10,10 +10,12 @@ use Doctrine\Common\Collections\Collection;
 use phpDocumentor\Reflection\Types\Integer;
 use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\Security\Core\User\UserInterface;
+use ApiPlatform\Core\Annotation\ApiResource;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
  * @ORM\Table(name="`user`")
+ * @ApiResource()
  */
 class User implements UserInterface
 {
