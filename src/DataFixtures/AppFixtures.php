b5197c2180dc0f23df3105641bea01ab4bec4797
Abdelhadi <Abdel@macairabdel.home>
2020-11-26 14:09:08 +0100

    Add fixtures user and article

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,42 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\Article;
+use Faker\Factory;
+use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder) 
+    {
+        $this->encoder = $encoder;
+    }
+
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $fake = Factory::create();
+        for ($i = 0; $i < 10; $i++) {
+            $user = new User();
+
+            $passwordHash = $this->encoder->encodePassword($user, 'password');
+            $user->setEmail($fake->email)
+            ->setPassword($passwordHash);
+
+            $manager->persist($user);
+
+            for ($a = 0; $a < random_int(5, 15); $a++) {
+                $article = (new Article())->setAuthor($user)
+                ->setContent($fake->text(300))
+                ->setName($fake->text(50));
+
+                $manager->persist($article);
+            }
+        }
 
         $manager->flush();
     }
