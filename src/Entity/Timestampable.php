9a4e92667c751904091728390c852a254ceff068
Abdelhadi <abdelhadi.microentreprise@gmail.com>
2020-11-30 12:06:09 +0100

    Add custom operation controllers

--- src/Entity/Timestampable.php
+++ src/Entity/Timestampable.php
@@ -4,18 +4,21 @@ declare(strict_types=1);
 namespace App\Entity;
 
 use DateTimeInterface;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 trait Timestampable 
 {
     /**
      * @var  DateTimeInterface
      * @ORM\Column(type="datetime")
+     * @Groups({"article_read", "article_details_read", "user_read", "user_details_read"})
      */
     private $createdAt;
 
     /**
      * @var  DateTimeInterface
      * @ORM\Column(type="datetime", nullable=true)
+     * @Groups({"article_read", "article_details_read"})
      */
     private $updatedAt;
 
